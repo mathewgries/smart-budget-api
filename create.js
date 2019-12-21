466ffaca2bac88f92d7290bb3873907d34e5bc50
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-21 18:15:26 -0500

    Serverless configurations

---
+++ create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            accountId: uuid.v1(),
+            description: data.description,
+            balance: data.startBalance,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
