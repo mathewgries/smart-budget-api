fc7c2d3e900d7691ad615c58bc7350b2160f443e
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-22 12:50:35 -0500

    Add get function

--- functions/get.js
+++ functions/get.js
@@ -1,23 +1,23 @@
-import uuid from "uuid";
 import * as dynamoDbLib from '../libs/dynamodb-lib';
 import { success, failure } from '../libs/response-lib';
 
 export async function main(event, context) {
-    const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
-        Item: {
+        Key: {
             userId: event.requestContext.identity.cognitoIdentityId,
-            accountId: uuid.v1(),
-            description: data.description,
-            balance: data.startBalance,
-            createdAt: Date.now()
+            accountId: event.pathParameters.id
         }
     };
 
     try {
-        await dynamoDbLib.call("put", params);
-        return success(params.Item);
+        const result = await dynamoDbLib.call("get", params);
+        if (result.Item) {
+            // Return the retrieved item
+            return success(result.Item);
+        } else {
+            return failure({ status: false, error: "Item not found." });
+        }
     } catch (e) {
         return failure({ status: false });
     }
