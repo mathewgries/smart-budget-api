8d078bdbe4f78c4dd9a0554e3c15e86679635ed1
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-23 11:39:39 -0500

    Add async keyword

--- functions/list.js
+++ functions/list.js
@@ -1,7 +1,7 @@
-import * as dynamodbLib from '../libs/dynamodb-lib';
+import * as dynamoDbLib from '../libs/dynamodb-lib';
 import { success, failure } from '../libs/response-lib';
 
-export default function main(event, context) {
+export async function main(event, context) {
     const params = {
         TableName: process.env.tableName,
         KeyConditionExpression: "userId = :userId",
