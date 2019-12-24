351ca1c6f617d28234a71321cb3a03b37b948aaf
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-23 21:07:34 -0500

    Fix 500 error for put part 1

--- functions/update.js
+++ functions/update.js
@@ -14,10 +14,11 @@ export async function main(event, context) {
         },
         // 'UpdateExpression' defines the attributes to be updated
         // 'ExpressionAttributeValues' defines the value in the update expression
-        UpdateExpression: "SET name = :name, description = :description",
+        UpdateExpression: "SET name = :name, description = :description, balance = :balance",
         ExpressionAttributeValues: {
             ":name": data.name || null,
-            ":description": data.description || null
+            ":description": data.description || null,
+            ":balance": data.balance || null
         },
         // 'ReturnValues' specifies if and how to return the item's attributes,
         // where ALL_NEW returns all attributes of the item after the update; you
