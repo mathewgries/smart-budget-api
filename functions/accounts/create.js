1ea1a17aaed3fd5289597c485fc6ceea299cdcdf
Gries, Matt <Matt.Gries@freedompay.com>
2020-01-12 20:37:32 -0500

    Update create entity 4

--- functions/accounts/create.js
+++ functions/accounts/create.js
@@ -2,17 +2,6 @@ import uuid from "uuid";
 import * as dynamoDbLib from '../../libs/dynamodb-lib';
 import { success, failure } from '../../libs/response-lib';
 
-function formatItem(data, event) {
-    switch (data.type) {
-        case 'account':
-            return formatAccount(data, event);
-        case 'goal':
-            return formatGoal(data, event);
-        default:
-            return null;
-    };
-}
-
 function formatAccount(data, event) {
     return {
         userId: event.requestContext.identity.cognitoIdentityId,
@@ -21,31 +10,21 @@ function formatAccount(data, event) {
         description: data.description,
         balance: data.balance,
         createdAt: Date.now()
-    };
-}
-
-function formatGoal(data, event) {
-    return {
-        userId: event.requestContext.identity.cognitoIdentityId,
-        goalId: uuid.v1(),
-        name: data.name,
-        description: data.description,
-        balance: data.balance,
-        createdAt: Date.now()
-    };
+    }
 }
 
 export async function main(event, context) {
     const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
-        Item: formatItem(data, event)
+        Item: formatAccount(data, event)
     };
 
     try {
         await dynamoDbLib.call("put", params);
         return success(params.Item);
     } catch (e) {
+        console.log(e);
         return failure({ status: false });
     }
 }
